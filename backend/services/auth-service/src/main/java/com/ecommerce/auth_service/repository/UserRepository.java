ab3bdd34114ba1d1bcde68a4d67a49bec25433b1
pavan.rathod <pavan.rathod@premierfarnell.com>
2026-01-22 09:45:03 +0530

    Configuring Auth Service

---
+++ backend/services/auth-service/src/main/java/com/ecommerce/auth_service/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.ecommerce.auth_service.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ecommerce.auth_service.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	Optional<User> findByEmail(String email);
+
+}
